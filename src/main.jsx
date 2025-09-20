0dbdcc1cc25ed6880ce51f2e17f0aac071f86c74
thuglyf501 <thuglyf501@gmail.com>
2025-09-20 22:06:43 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import SplashCursor from './Components/SplashCursor.jsx'
 
 import { BrowserRouter, Routes, Route } from "react-router";
